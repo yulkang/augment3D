a43245e07ced2bd0e0f5c350a85001b2b39febb0
YulKang <yul.kang.on@gmail.com>
2016-10-17 22:04:45 -0400

    made compare_cand_vs_annot as a module and separated match_cand

--- compare_cand_vs_annot.py
+++ compare_cand_vs_annot.py
@@ -16,21 +16,16 @@ annotation_file = 'Data/LUNA/annotations.csv'
 cands = pd.read_csv(cand_file)
 cands_pos = pd.read_csv(annotation_file)
 
-#%% Find pos
-ix = np.nonzero(cands['class'])
+#%% Compare cands and cands_pos
+def main():
+    for row in range(len(cands)):
+        check_cand_w_annot(cands.iloc[row])
 
-#%% 
+#%% Check
 def check_cand_w_annot(cand):
-    same_uid = np.nonzero(cands_pos['seriesuid'] == cand['seriesuid'])[0]
-    cand_pos_same_uid = cands_pos.iloc[same_uid,:]
-
-    d_coord = np.array(cand_pos_same_uid[['coordX', 'coordY', 'coordZ']]) \
-            - np.array(cand[['coordX', 'coordY', 'coordZ']])
-    dist = np.sum(d_coord ** 2, 1) ** 0.5
-    radius = np.array(cand_pos_same_uid['diameter_mm']) / 2
-    is_pos = dist <= radius
-
-    if cand['class'] != np.any(is_pos):
+    any_pos, is_pos, dist, radius = match_cand(cand)
+    
+    if cand['class'] != any_pos:
         print(cand)
         raise ValueError('Discrepancy of class was found')
     elif cand['class'] == 1:
@@ -39,7 +34,16 @@ def check_cand_w_annot(cand):
             print('dist %1.1f <= radius %1.1f' % 
                   (dist[is_pos1], radius[is_pos1]))
             
-#%% Compare cands and cands_pos
-for row in range(len(cands)):
-    check_cand_w_annot(cands.iloc[row])
-        
+#%% 
+def match_cand(cand):
+    same_uid = np.nonzero(cands_pos['seriesuid'] == cand['seriesuid'])[0]
+    cand_pos_same_uid = cands_pos.iloc[same_uid,:]
+
+    d_coord = np.array(cand_pos_same_uid[['coordX', 'coordY', 'coordZ']]) \
+            - np.array(cand[['coordX', 'coordY', 'coordZ']])
+    dist = np.sum(d_coord ** 2, 1) ** 0.5
+    radius = np.array(cand_pos_same_uid['diameter_mm']) / 2
+    is_pos = dist <= radius
+    
+    return any(is_pos), is_pos, dist, radius, d_coord
+    
