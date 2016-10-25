dfc2727981ff3158348c36b7f5184864010b9609
YulKang <yul.kang.on@gmail.com>
2016-10-25 19:33:10 -0400

    debugged img_size; now it loads

--- classify_patch.py
+++ classify_patch.py
@@ -97,6 +97,7 @@ def load_cand(cands, scale=0):
             
     if img_all is not None:
         img_all = img_all[:n_loaded,:,:,:]
+        img_all = img_all.reshape(np.concatenate((siz,[1])))
         ix_loaded = ix_loaded[:n_loaded]
         ix_loaded = cands.index[ix_loaded]
         label_all = cands.loc[ix_loaded,'is_pos']
@@ -110,7 +111,7 @@ def load_cand(cands, scale=0):
         
     print('Last image loaded:')
     print(np.int32(ix_loaded[-1])) # DEBUG    
-    patch1 = img_all[-1,img_all.shape[1]/2,:,:]
+    patch1 = img_all[-1,img_all.shape[1]/2,:,:,0]
     plt.imshow(patch1, cmap='gray')
     plt.show()
     
@@ -120,130 +121,39 @@ img_neg, label_neg, ix_loaded_neg = load_cand(cands_neg.iloc[:,:])
 img_pos, label_pos, ix_loaded_pos = load_cand(cands_pos.iloc[:,:])
 
 #%%
-image_size = 28
-num_labels = 10
-num_channels = 1 # grayscale
-
-def reformat(dataset, labels):
-  dataset = dataset.reshape(
-    (-1, image_size, image_size, num_channels)).astype(np.float32)
-  labels = (np.arange(num_labels) == labels[:,None]).astype(np.float32)
-  return dataset, labels
-train_dataset, train_labels = reformat(train_dataset, train_labels)
-valid_dataset, valid_labels = reformat(valid_dataset, valid_labels)
-test_dataset, test_labels = reformat(test_dataset, test_labels)
-print('Training set', train_dataset.shape, train_labels.shape)
-print('Validation set', valid_dataset.shape, valid_labels.shape)
-print('Test set', test_dataset.shape, test_labels.shape)
+img_size = img_neg.shape[0]
+n_labels = 2
+n_chan = 1 # grayscale
 
 #%%
 def accuracy(predictions, labels):
-  return (100.0 * np.sum(np.argmax(predictions, 1) == np.argmax(labels, 1))
-          / predictions.shape[0])
+  return (100.0 * np.mean(predictions == labels))
   
-#%%
-batch_size = 16
-patch_size = 5
-depth = 16
-num_hidden = 64
-
-graph = tf.Graph()
-
-with graph.as_default():
-
-  # Input data.
-  tf_train_dataset = tf.placeholder(
-    tf.float32, shape=(batch_size, image_size, image_size, num_channels))
-  tf_train_labels = tf.placeholder(tf.float32, shape=(batch_size, num_labels))
-  tf_valid_dataset = tf.constant(valid_dataset)
-  tf_test_dataset = tf.constant(test_dataset)
-  
-  # Variables.
-  layer1_weights = tf.Variable(tf.truncated_normal(
-      [patch_size, patch_size, num_channels, depth], stddev=0.1))
-  layer1_biases = tf.Variable(tf.zeros([depth]))
-  layer2_weights = tf.Variable(tf.truncated_normal(
-      [patch_size, patch_size, depth, depth], stddev=0.1))
-  layer2_biases = tf.Variable(tf.constant(1.0, shape=[depth]))
-  layer3_weights = tf.Variable(tf.truncated_normal(
-      [image_size // 4 * image_size // 4 * depth, num_hidden], stddev=0.1))
-  layer3_biases = tf.Variable(tf.constant(1.0, shape=[num_hidden]))
-  layer4_weights = tf.Variable(tf.truncated_normal(
-      [num_hidden, num_labels], stddev=0.1))
-  layer4_biases = tf.Variable(tf.constant(1.0, shape=[num_labels]))
-  
-  # Model.
-  def model(data):
-    conv = tf.nn.conv2d(data, layer1_weights, [1, 2, 2, 1], padding='SAME')
-    hidden = tf.nn.relu(conv + layer1_biases)
-    conv = tf.nn.conv2d(hidden, layer2_weights, [1, 2, 2, 1], padding='SAME')
-    hidden = tf.nn.relu(conv + layer2_biases)
-    shape = hidden.get_shape().as_list()
-    reshape = tf.reshape(hidden, [shape[0], shape[1] * shape[2] * shape[3]])
-    hidden = tf.nn.relu(tf.matmul(reshape, layer3_weights) + layer3_biases)
-    return tf.matmul(hidden, layer4_weights) + layer4_biases
-  
-  # Training computation.
-  logits = model(tf_train_dataset)
-  loss = tf.reduce_mean(
-    tf.nn.softmax_cross_entropy_with_logits(logits, tf_train_labels))
-    
-  # Optimizer.
-  optimizer = tf.train.GradientDescentOptimizer(0.05).minimize(loss)
-  
-  # Predictions for the training, validation, and test data.
-  train_prediction = tf.nn.softmax(logits)
-  valid_prediction = tf.nn.softmax(model(tf_valid_dataset))
-  test_prediction = tf.nn.softmax(model(tf_test_dataset))
-  
-#%%
-num_steps = 1001
-
-with tf.Session(graph=graph) as session:
-  tf.initialize_all_variables().run()
-  print('Initialized')
-  for step in range(num_steps):
-    offset = (step * batch_size) % (train_labels.shape[0] - batch_size)
-    batch_data = train_dataset[offset:(offset + batch_size), :, :, :]
-    batch_labels = train_labels[offset:(offset + batch_size), :]
-    feed_dict = {tf_train_dataset : batch_data, tf_train_labels : batch_labels}
-    _, l, predictions = session.run(
-      [optimizer, loss, train_prediction], feed_dict=feed_dict)
-    if (step % 50 == 0):
-      print('Minibatch loss at step %d: %f' % (step, l))
-      print('Minibatch accuracy: %.1f%%' % accuracy(predictions, batch_labels))
-      print('Validation accuracy: %.1f%%' % accuracy(
-        valid_prediction.eval(), valid_labels))
-  print('Test accuracy: %.1f%%' % accuracy(test_prediction.eval(), test_labels))
-  
-
-#%% Problem 1
-# Problem 1
-# The convolutional model above uses convolutions with stride 2 
-# to reduce the dimensionality. Replace the strides by a max pooling 
-# operation (nn.max_pool()) of stride 2 and kernel size 2.
-
 #%%
 batch_size = 128 # 16
-patch_size = 5
-depth = 16
-num_hidden = 64
+patch_size = 4
+depth = 64
+n_hidden = 64
   
+#%%
+if 'sess' in vars():
+    sess.close()
+    
 sess = tf.InteractiveSession()
 
-# Input data.
+#%% Input data.
 x = tf.placeholder(tf.float32, 
-                   shape=[None, image_size, image_size, num_channels])
+                   shape=[None, img_size, img_size, img_size, n_chan])
 y = tf.placeholder(tf.float32, 
-                   shape=[None, num_labels])
+                   shape=[None, n_labels])
 
 #tf_train_dataset = tf.placeholder(
-#  tf.float32, shape=(batch_size, image_size, image_size, num_channels))
-#tf_train_labels = tf.placeholder(tf.float32, shape=(batch_size, num_labels))
+#  tf.float32, shape=(batch_size, img_size, img_size, n_chan))
+#tf_train_labels = tf.placeholder(tf.float32, shape=(batch_size, n_labels))
 #tf_valid_dataset = tf.constant(valid_dataset)
 #tf_test_dataset = tf.constant(test_dataset)
 
-# Variables.
+#%% Variables.
 keep_prob_train = [.5, 1, 1, 1]
 keep_all = [1, 1, 1, 1]
 
@@ -252,7 +162,7 @@ stride_conv = [1, 1, 0, 0]
 width_conv = [5, 5, 0, 0]
 width_pool = [2, 2, 0, 0]
 depth_out = [16, 16, 16, 16]
-depth_in = num_channels + depth_out[:-1]
+depth_in = n_chan + depth_out[:-1]
 
 weights0 = list()
 biases = list()
@@ -274,7 +184,7 @@ for layer in range(n_layer):
     
 
 weights0.append(tf.Variable(tf.truncated_normal(
-    [patch_size, patch_size, num_channels, depth_conv[0]], stddev=0.1)))
+    [patch_size, patch_size, n_chan, depth_conv[0]], stddev=0.1)))
 biases.append(tf.Variable(tf.zeros([depth_conv[0]])))
 
 weights0.append(tf.Variable(tf.truncated_normal(
@@ -282,13 +192,13 @@ weights0.append(tf.Variable(tf.truncated_normal(
 biases.append(tf.Variable(tf.constant(1.0, shape=[depth])))
 
 weights0.append(tf.Variable(tf.truncated_normal(
-    [image_size // stride_conv[0]**2 * image_size // stride_conv[0]**2 * depth, 
-     num_hidden], stddev=0.1)))
-biases.append(tf.Variable(tf.constant(1.0, shape=[num_hidden])))
+    [img_size // stride_conv[0]**2 * img_size // stride_conv[0]**2 * depth, 
+     n_hidden], stddev=0.1)))
+biases.append(tf.Variable(tf.constant(1.0, shape=[n_hidden])))
 
 weights0.append(tf.Variable(tf.truncated_normal(
-    [num_hidden, num_labels], stddev=0.1)))
-biases.append(tf.Variable(tf.constant(1.0, shape=[num_labels])))
+    [n_hidden, n_labels], stddev=0.1)))
+biases.append(tf.Variable(tf.constant(1.0, shape=[n_labels])))
 
 # Model.
 def add_conv2d(inp, weight, bias,
