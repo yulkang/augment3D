577469a86a7a9861780dfb171506f0c221e105b0
YulKang <yul.kang.on@gmail.com>
2017-01-11 13:17:19 -0500

    updated paths

--- paths.py
+++ paths.py
@@ -9,18 +9,18 @@ Created on Thu Dec 15 04:32:01 2016
 """
 
 # Input - small
-cand_file = '../Data/LUNA/candidates.csv'
-annotation_file = '../Data/LUNA/annotations.csv'
-uid_subset_file = '../Data/LUNA/uid_subset.csv'
+cand_file = 'Data/LUNA/candidates.csv'
+annotation_file = 'Data/LUNA/annotations.csv'
+uid_subset_file = 'Data/LUNA/uid_subset.csv'
 
 # Input - large
 img_dir_root = '/Volumes/YK_SSD_1TB/LUNA/'
 img_dir = '/Volumes/YK_SSD_1TB/LUNA/all'
 
 # Output - small
-subset_file = '../Data/LUNA/uid_subset.csv'
-meta_file = '../Data/LUNA/img_meta.csv'
-cand_out_file = '../Data/LUNA/candidates_vs_annot.csv'
+subset_file = 'Data/LUNA/uid_subset.csv'
+meta_file = 'Data/LUNA/img_meta.csv'
+cand_out_file = 'Data/LUNA/candidates_vs_annot.csv'
 
 # Output - large
 patch_dir = '/Volumes/YK_SSD_1TB/LUNA/patches'
